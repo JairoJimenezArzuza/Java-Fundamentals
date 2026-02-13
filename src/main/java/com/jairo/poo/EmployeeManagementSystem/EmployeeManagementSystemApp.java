50a5e5e117f4f6ba5c57fccda839890afaab716e
Jairo Jimenez <jairodanieljimeneza2025@gmail.com>
2026-02-12 19:19:45 -0500

    Creación de un main en el proyecto EmployeeManagementSystem en la clase EmployeeManagementSystem para realizar las primeras pruebas unitarias con JUnit

--- src/main/java/com/jairo/poo/EmployeeManagementSystem/EmployeeManagementSystemApp.java
+++ src/main/java/com/jairo/poo/EmployeeManagementSystem/EmployeeManagementSystemApp.java
@@ -1,8 +1,16 @@
 package com.jairo.poo.EmployeeManagementSystem;
 
+import java.util.List;
+import java.math.BigDecimal;
 
 public class EmployeeManagementSystemApp{
     public static void main(String[] args) {
-        System.out.println("sssss");
+        List<Empleado> lista = List.of(
+            new EmpleadoAsalariado(new BigDecimal("3000.00"),1, "Jairo", "Sistemas" ),
+            new EmpleadoAsalariado(new BigDecimal("3000.00"),2, "Gabo", "Relaciones publicas" )    
+        );
+
+        NominaService service = new NominaService(lista);
+        System.out.println(service.ejecutarPagoEmpleados());
     }
 }
--- src/main/java/com/jairo/poo/EmployeeManagementSystem/NominaService.java
+++ src/main/java/com/jairo/poo/EmployeeManagementSystem/NominaService.java
@@ -1,6 +1,33 @@
 
 package com.jairo.poo.EmployeeManagementSystem;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class NominaService{
+    private final List<Empleado> listaEmpleados;
+
+    public NominaService(List<Empleado> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
     
+    public String ejecutarPagoEmpleados(){
+        StringBuilder mensaje = new StringBuilder("=== REPORTE DE PAGO ===\n");
+        BigDecimal sueldoTotalCalculado = BigDecimal.ZERO;
+        for(Empleado ls:listaEmpleados){
+            mensaje.append("\n");
+            BigDecimal sueldoBig = ls.calcularSueldo();
+       
+            if (sueldoBig.compareTo(BigDecimal.ZERO) <= 0) {
+               throw  new IllegalArgumentException("Error: Empleado "+ls.getId()+" Tiene un salario no valido "+sueldoBig);
+            }else{
+                mensaje.append("Trabajador ").append(ls.getId())
+                   .append(": $").append(sueldoBig);
+                sueldoTotalCalculado = sueldoTotalCalculado.add(sueldoBig);
+            }
+        }
+        mensaje.append("\n--------------------------\n").append(sueldoTotalCalculado);
+    return mensaje.toString();        
+    }
+
 }
